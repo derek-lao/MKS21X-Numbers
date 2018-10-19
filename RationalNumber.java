23f58da4d6a046e0154a8fd2ad3c9c4c0d14beec
Derek Lao <dlao00@stuy.edu>
2018-10-19 03:14:02 -0400

    added a little bit of code in RationalNumber to make an input of RationalNumber(integer,0) will return me 0

--- RationalNumber.java
+++ RationalNumber.java
@@ -7,8 +7,16 @@ public class RationalNumber extends RealNumber{
   */
   public RationalNumber(int nume, int deno){
     super(0.0);//this value is ignored!
-    numerator=nume;
-    denominator=deno;
+    if(deno==0)
+    {
+      numerator=0;
+      denominator=1;
+    }
+    else
+    {
+      numerator=nume;
+      denominator=deno;
+    }
     this.reduce();
   }
 
